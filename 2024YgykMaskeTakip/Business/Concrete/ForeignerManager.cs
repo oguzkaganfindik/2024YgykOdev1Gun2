c922f789ca2ebb618c36feb8026d017d2af0d43e
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-02-16 17:52:09 +0300

    Odev Dahilinde Maske Takip Uygulamasi Yapildi

---
+++ 2024YgykMaskeTakip/Business/Concrete/ForeignerManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class ForeignerManager : IApplicantService
+    {
+        // SonarQube
+        public void ApplyForMask(Person person)
+        {
+            throw new System.NotImplementedException();
+        }
+        public List<Person> GetList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
